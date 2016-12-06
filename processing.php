3e71013e2c5bee6546c3eb6ea7aba2e19a723ba9
Martin Melka <melkamar@fit.cvut.cz>
2016-12-06 23:14:35 +0100

    Completed duration and original-position weighing and ranking. Working!

--- processing.php
+++ processing.php
@@ -8,6 +8,7 @@
 
 include_once 'core.php';
 include_once 'util.php';
+include_once 'normalizing.php';
 
 /**
  * @param \Video[] $resultCollection Array of \Video objects to be reranked.
@@ -22,7 +23,9 @@ function rerankResultCollection($resultCollection, $params)
 
     // Calculate non-normalized distances for given videos,
     // keep track of maxims to later rerank.
-    $rerankings = array();
+    /** @var \MetaVideo[] $metaVideos */
+    $metaVideos = array();
+    $maxOriginalPosition = null;
     $maxDurationDistance = null;
     $maxDatePublishedDistance = null;
     $maxGpsDistance = null;
@@ -33,58 +36,65 @@ function rerankResultCollection($resultCollection, $params)
     foreach ($resultCollection as $video) {
         debug_log(">>> Processing video " . $video->getId());
 
-        $reranking = new VideoReranking();
-        $reranking->setDurationDistance(calcDurationDistance($video, $params));
-        $reranking->setDatePublishedDistance(calcDatePublishedDistance($video, $params));
-        $reranking->setGpsDistance(calcGpsDistance($video, $params));
-        $reranking->setViewsDistance(calcViewsDistance($video, $params));
-        $reranking->setTudRatioDistance(calcTudRatioDistance($video, $params));
-        $reranking->setAuthorNameDistance(calcAuthorNameDistance($video, $params));
+        $metaVideo = new MetaVideo($video);
+        $metaVideo->setDurationDistance(calcDurationDistance($video, $params));
+        $metaVideo->setDatePublishedDistance(calcDatePublishedDistance($video, $params));
+        $metaVideo->setGpsDistance(calcGpsDistance($video, $params));
+        $metaVideo->setViewsDistance(calcViewsDistance($video, $params));
+        $metaVideo->setTudRatioDistance(calcTudRatioDistance($video, $params));
+        $metaVideo->setAuthorNameDistance(calcAuthorNameDistance($video, $params));
+
+        if ($maxOriginalPosition === null) {
+            $maxOriginalPosition = $metaVideo->getOriginalPosition();
+        } else {
+            if ($metaVideo->getOriginalPosition() !== null && $metaVideo->getOriginalPosition() > $maxOriginalPosition)
+                $maxOriginalPosition = $metaVideo->getOriginalPosition();
+        }
 
         if ($maxDurationDistance === null) {
-            $maxDurationDistance = $reranking->getDurationDistance();
+            $maxDurationDistance = $metaVideo->getDurationDistance();
         } else {
-            if ($reranking->getDurationDistance() !== null && $reranking->getDurationDistance() > $maxDurationDistance)
-                $maxDurationDistance = $reranking->getDurationDistance();
+            if ($metaVideo->getDurationDistance() !== null && $metaVideo->getDurationDistance() > $maxDurationDistance)
+                $maxDurationDistance = $metaVideo->getDurationDistance();
         }
 
         if ($maxDatePublishedDistance === null) {
-            $maxDatePublishedDistance = $reranking->getDatePublishedDistance();
+            $maxDatePublishedDistance = $metaVideo->getDatePublishedDistance();
         } else {
-            if ($reranking->getDatePublishedDistance() !== null && $reranking->getDatePublishedDistance() > $maxDatePublishedDistance)
-                $maxDatePublishedDistance = $reranking->getDatePublishedDistance();
+            if ($metaVideo->getDatePublishedDistance() !== null && $metaVideo->getDatePublishedDistance() > $maxDatePublishedDistance)
+                $maxDatePublishedDistance = $metaVideo->getDatePublishedDistance();
         }
 
         if ($maxGpsDistance === null) {
-            $maxGpsDistance = $reranking->getGpsDistance();
+            $maxGpsDistance = $metaVideo->getGpsDistance();
         } else {
-            if ($reranking->getGpsDistance() !== null && $reranking->getGpsDistance() > $maxGpsDistance)
-                $maxGpsDistance = $reranking->getGpsDistance();
+            if ($metaVideo->getGpsDistance() !== null && $metaVideo->getGpsDistance() > $maxGpsDistance)
+                $maxGpsDistance = $metaVideo->getGpsDistance();
         }
 
         if ($maxViewsDistance === null) {
-            $maxViewsDistance = $reranking->getViewsDistance();
+            $maxViewsDistance = $metaVideo->getViewsDistance();
         } else {
-            if ($reranking->getViewsDistance() !== null && $reranking->getViewsDistance() > $maxViewsDistance)
-                $maxViewsDistance = $reranking->getViewsDistance();
+            if ($metaVideo->getViewsDistance() !== null && $metaVideo->getViewsDistance() > $maxViewsDistance)
+                $maxViewsDistance = $metaVideo->getViewsDistance();
         }
 
         if ($maxTudRatioDistance === null) {
-            $maxTudRatioDistance = $reranking->getTudRatioDistance();
+            $maxTudRatioDistance = $metaVideo->getTudRatioDistance();
         } else {
-            if ($reranking->getTudRatioDistance() !== null && $reranking->getTudRatioDistance() > $maxTudRatioDistance)
-                $maxTudRatioDistance = $reranking->getTudRatioDistance();
+            if ($metaVideo->getTudRatioDistance() !== null && $metaVideo->getTudRatioDistance() > $maxTudRatioDistance)
+                $maxTudRatioDistance = $metaVideo->getTudRatioDistance();
         }
 
         if ($maxAuthorNameDistance === null) {
-            $maxAuthorNameDistance = $reranking->getAuthorNameDistance();
+            $maxAuthorNameDistance = $metaVideo->getAuthorNameDistance();
         } else {
-            if ($reranking->getAuthorNameDistance() !== null && $reranking->getAuthorNameDistance() > $maxAuthorNameDistance)
-                $maxAuthorNameDistance = $reranking->getAuthorNameDistance();
+            if ($metaVideo->getAuthorNameDistance() !== null && $metaVideo->getAuthorNameDistance() > $maxAuthorNameDistance)
+                $maxAuthorNameDistance = $metaVideo->getAuthorNameDistance();
         }
 
 
-        array_push($rerankings, $reranking);
+        array_push($metaVideos, $metaVideo);
 
         debug_log("");
         debug_log("");
@@ -94,20 +104,273 @@ function rerankResultCollection($resultCollection, $params)
     debug_log("");
     debug_log(">>> Normalizing...");
 
+    $maxims = new DistanceMaxims($maxOriginalPosition,
+        $maxDurationDistance,
+        $maxDatePublishedDistance,
+        $maxGpsDistance,
+        $maxViewsDistance,
+        $maxTudRatioDistance,
+        $maxAuthorNameDistance);
+
+    normalize($metaVideos, $maxims);
+    computeScores($metaVideos, $params);
+
+    debug_log("############################################");
+    debug_log("Original sorting:");
+    foreach ($metaVideos as $metaVideo) {
+        debug_log(" - score: " . $metaVideo->getScore() . "  ID: " . $metaVideo->getVideo()->getId());
+    }
+
+    if (!usort($metaVideos, 'compareMetaVideos')) {
+        debug_log("COULD NOT SORT!");
+    }
+
+    debug_log("############################################");
+    debug_log("Reranked videos:");
+    $result = array();
+    foreach ($metaVideos as $metaVideo) {
+        array_push($result, $metaVideo->getVideo());
+        debug_log(" - score: " . $metaVideo->getScore() . "  ID: " . $metaVideo->getVideo()->getId());
+    }
+
+    return $result;
 }
 
-class VideoReranking
+/**
+ * @param \MetaVideo[] $metaVideos
+ * @param \RerankParams $params
+ */
+function computeScores($metaVideos, $params)
+{
+    foreach ($metaVideos as $metaVideo) {
+        $metaVideo->invertDistances();
+        $metaVideo->recalculateScore($params);
+        debug_log("  ### video score: " . $metaVideo->getScore());
+        debug_log("");
+    }
+}
+
+/**
+ * @param \MetaVideo $metaVideoA
+ * @param \MetaVideo $metaVideoB
+ * @return mixed
+ */
+function compareMetaVideos($metaVideoA, $metaVideoB)
+{
+    return sign($metaVideoB->getScore() - $metaVideoA->getScore());
+}
+
+/**
+ * Get sign of a number.
+ * @param $number
+ * @return int -1, 0 or 1.
+ */
+function sign($number)
+{
+    return ($number > 0) ? 1 : (($number < 0) ? -1 : 0);
+}
+
+
+class DistanceMaxims
+{
+    public $maxOriginalPosition;
+    public $maxDurationDistance;
+    public $maxDatePublishedDistance;
+    public $maxGpsDistance;
+    public $maxViewsDistance;
+    public $maxTudRatioDistance;
+    public $maxAuthorNameDistance;
+
+    /**
+     * DistanceMaxims constructor.
+     * @param $maxOriginalPosition
+     * @param $maxDurationDistance
+     * @param $maxDatePublishedDistance
+     * @param $maxGpsDistance
+     * @param $maxViewsDistance
+     * @param $maxTudRatioDistance
+     * @param $maxAuthorNameDistance
+     */
+    public function __construct($maxOriginalPosition,
+                                $maxDurationDistance,
+                                $maxDatePublishedDistance,
+                                $maxGpsDistance,
+                                $maxViewsDistance,
+                                $maxTudRatioDistance,
+                                $maxAuthorNameDistance)
+    {
+        $this->maxOriginalPosition = $maxOriginalPosition;
+        $this->maxDurationDistance = $maxDurationDistance;
+        $this->maxDatePublishedDistance = $maxDatePublishedDistance;
+        $this->maxGpsDistance = $maxGpsDistance;
+        $this->maxViewsDistance = $maxViewsDistance;
+        $this->maxTudRatioDistance = $maxTudRatioDistance;
+        $this->maxAuthorNameDistance = $maxAuthorNameDistance;
+    }
+
+
+}
+
+class MetaVideo
 {
     private $video;
+    private $originalPosition;
     private $durationDistance;
     private $datePublishedDistance;
     private $gpsDistance;
     private $viewsDistance;
     private $tudRatioDistance;
     private $authorNameDistance;
+    private $score;
+
+    /**
+     * MetaVideo constructor.
+     * @param \Video $video
+     */
+    public function __construct($video)
+    {
+        $this->video = $video;
+        $this->originalPosition = $video->getResultStanding();
+
+        debug_log(" New MetaVideo, position: " . $this->originalPosition);
+    }
+
+    /**
+     * Invert distance values. 0 becomes 1, 1 becomes 0.
+     */
+    public function invertDistances()
+    {
+        $this->originalPosition = 1 - $this->originalPosition;
+        $this->durationDistance = 1 - $this->durationDistance;
+        $this->datePublishedDistance = 1 - $this->datePublishedDistance;
+        $this->gpsDistance = 1 - $this->gpsDistance;
+        $this->viewsDistance = 1 - $this->viewsDistance;
+        $this->tudRatioDistance = 1 - $this->tudRatioDistance;
+        $this->authorNameDistance = 1 - $this->authorNameDistance;
+    }
+
+    /**
+     * Refresh the $score attribute with given data.
+     * @param \RerankParams $params
+     */
+    public function recalculateScore($params)
+    {
+        $this->score = 0;
+        if ($params->getOriginalPositionWeight() === null) {
+            debug_log("  Ignoring score from ResStanding, its weight is null.");
+        } else if ($this->originalPosition === null) {
+            debug_log("  Ignoring score from ResStanding, its calculated distance is null.");
+        } else {
+            $scoreInc = $this->originalPosition * $params->getOriginalPositionWeight();
+            debug_log("  Adding score from ResStanding: " . $scoreInc);
+            $this->score += $scoreInc;
+
+        }
+
+        if ($params->getDurationWeight() === null) {
+            debug_log("  Ignoring score from Duration, its weight is null.");
+        } else if ($this->durationDistance === null) {
+            debug_log("  Ignoring score from Duration, its calculated distance is null.");
+        } else {
+            $scoreInc = $this->durationDistance * $params->getDurationWeight();
+            debug_log("  Adding score from Duration: " . $scoreInc);
+            $this->score += $scoreInc;
+
+        }
+
+        if ($params->getDatePublishedWeight() === null) {
+            debug_log("  Ignoring score from DatePublished, its weight is null.");
+        } else if ($this->datePublishedDistance === null) {
+            debug_log("  Ignoring score from DatePublished, its calculated distance is null.");
+        } else {
+            $scoreInc = $this->datePublishedDistance * $params->getDatePublishedWeight();
+            debug_log("  Adding score from DatePublished: " . $scoreInc);
+            $this->score += $scoreInc;
+
+        }
+
+        if ($params->getGpsWeight() === null) {
+            debug_log("  Ignoring score from Gps, its weight is null.");
+        } else if ($this->gpsDistance === null) {
+            debug_log("  Ignoring score from Gps, its calculated distance is null.");
+        } else {
+            $scoreInc = $this->gpsDistance * $params->getGpsWeight();
+            debug_log("  Adding score from Gps: " . $scoreInc);
+            $this->score += $scoreInc;
+
+        }
+
+        if ($params->getViewsWeight() === null) {
+            debug_log("  Ignoring score from Views, its weight is null.");
+        } else if ($this->viewsDistance === null) {
+            debug_log("  Ignoring score from Views, its calculated distance is null.");
+        } else {
+            $scoreInc = $this->viewsDistance * $params->getViewsWeight();
+            debug_log("  Adding score from Views: " . $scoreInc);
+            $this->score += $scoreInc;
+
+        }
+
+        if ($params->getTudRatioWeight() === null) {
+            debug_log("  Ignoring score from TudRatio, its weight is null.");
+        } else if ($this->tudRatioDistance === null) {
+            debug_log("  Ignoring score from TudRatio, its calculated distance is null.");
+        } else {
+            $scoreInc = $this->tudRatioDistance * $params->getTudRatioWeight();
+            debug_log("  Adding score from TudRatio: " . $scoreInc);
+            $this->score += $scoreInc;
+
+        }
+
+        if ($params->getAuthorNameWeight() === null) {
+            debug_log("  Ignoring score from AuthorName, its weight is null.");
+        } else if ($this->authorNameDistance === null) {
+            debug_log("  Ignoring score from AuthorName, its calculated distance is null.");
+        } else {
+            $scoreInc = $this->authorNameDistance * $params->getAuthorNameWeight();
+            debug_log("  Adding score from AuthorName: " . $scoreInc);
+            $this->score += $scoreInc;
+
+        }
+
+    }
+
 
     /**
      * @return mixed
+     */
+    public function getScore()
+    {
+        return $this->score;
+    }
+
+    /**
+     * @param mixed $score
+     */
+    public function setScore($score)
+    {
+        $this->score = $score;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOriginalPosition()
+    {
+        return $this->originalPosition;
+    }
+
+    /**
+     * @param mixed $originalPosition
+     */
+    public function setOriginalPosition($originalPosition)
+    {
+        $this->originalPosition = $originalPosition;
+    }
+
+
+    /**
+     * @return \Video
      */
     public function getVideo()
     {
